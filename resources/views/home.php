fb203c73998442551ee75ca9c486e14c11dccf8d
mihai-valentin <mihaivalentin373@gmail.com>
2020-10-03 15:31:35 +0300

    Config  - Paths to imgs

--- resources/views/home.php
+++ resources/views/home.php
@@ -6,8 +6,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Ciphers</title>
-    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="/public/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/public/favicon.ico" type="image/x-icon">
     <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body>
@@ -15,7 +15,7 @@
     <div class="m-2">
         <a href="/cipher/caesar" class="flex py-2 px-5 bg-gray-400 rounded-lg shadow-xl">
             <div class="flex-shrink-0">
-                <img class="h-12" src="/img/caesar.jpg" alt="Caesar">
+                <img class="h-12" src="/public/img/caesar.jpg" alt="Caesar">
             </div>
             <div class="ml-3 flex items-center">
                 <h4 class="text-xl text-gray-900">Caesar cipher</h4>
